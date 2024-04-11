6ed4ff7f439bd0a27ac9dd03921c05564c7f508c
Samarth Asthan <97031010+samarthasthan@users.noreply.github.com>
2024-04-11 20:55:13 +0530

    Update code-server.sh

--- code-server.sh
+++ code-server.sh
@@ -61,5 +61,31 @@ password: Romioisno1.
 cert: false
 EOF
 
+sudo add-apt-repository ppa:longsleep/golang-backports
+sudo apt-get update
+sudo apt-get install -y golang-go 
+
+sudo apt install -y protobuf-compiler
+
+wget -qO- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
+source ~/.bashrc
+nvm install node
+
+go install google.golang.org/protobuf/cmd/protoc-gen-go@v1.28
+go install google.golang.org/grpc/cmd/protoc-gen-go-grpc@v1.2
+
+export PATH="$PATH:$(go env GOPATH)/bin"
+
+sudo apt install -y apt-transport-https ca-certificates curl software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable" 
+apt-cache policy docker-ce
+sudo apt install -y docker-ce 
+
+sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+docker-compose --version
+
+
 # Start code-server
 screen -S code-server -dm code-server --proxy-domain vscode.samarthasthan.com
