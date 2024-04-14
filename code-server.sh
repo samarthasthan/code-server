dce6bd08bb2daa9d100c484e5e81b4fd6433ca25
Samarth Asthan <97031010+samarthasthan@users.noreply.github.com>
2024-04-14 14:13:45 +0530

    Update code-server.sh

--- code-server.sh
+++ code-server.sh
@@ -89,6 +89,11 @@ docker-compose --version
 
 sudo apt install -y clang-format
 
+curl -L https://packagecloud.io/golang-migrate/migrate/gpgkey | apt-key add -
+echo "deb https://packagecloud.io/golang-migrate/migrate/ubuntu/ $(lsb_release -sc) main" > /etc/apt/sources.list.d/migrate.list
+apt-get update
+apt-get install -y migrate
+
 
 go install github.com/sqlc-dev/sqlc/cmd/sqlc@latest
 
